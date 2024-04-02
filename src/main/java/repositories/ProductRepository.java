7dbd86e229e2371fa7f1452aa21ee7cb2f7556f1
Matheos Profirescu <matheosprofirescu@yahoo.com>
2024-04-02 14:17:44 +0300

    jpa repositories for all entities

---
+++ src/main/java/repositories/ProductRepository.java
@@ -0,0 +1,6 @@
+package repositories;
+import entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
