6873d19eaeb36d78f2fea874c0983133fa490053
Matheos Profirescu <matheosprofirescu@yahoo.com>
2024-04-03 12:46:39 +0300

    get all products and get product by id

---
+++ src/main/java/com/example/coffeeShop/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.coffeeShop.controllers;
+
+import com.example.coffeeShop.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.ProductService;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
---
+++ src/main/java/com/example/coffeeShop/services/ProductService.java
@@ -0,0 +1,25 @@
+package services;
+
+import com.example.coffeeShop.entities.Product;
+import com.example.coffeeShop.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Integer id) {
+        return productRepository.findById(id);
+    }
+
+
+}
